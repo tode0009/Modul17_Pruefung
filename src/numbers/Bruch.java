43c1817f052e32521dd93a3773f94dfa9bc12d4d
Tolga Demirci <tode0009@stud.hs-kl.de>
2018-09-11 18:48:08 +0200

    Klasse Bruch.java erstellt, Programmiert und getestet.

---
+++ src/numbers/Bruch.java
@@ -0,0 +1,14 @@
+package numbers;
+
+public class Bruch {
+
+	public static void main(String[] args) {
+
+		System.out.println("Dieses Programm kürzt Ihren eingegebenen Bruch \n");
+		
+		Rational r = new Rational();
+		r.eingabe();
+
+	}
+
+}
