87090ffc5af689840a04028a12989e05152a1593
Tolga Demirci <tode0009@stud.hs-kl.de>
2018-09-13 16:15:54 +0200

    Datei dokumentiert, da keine Quellcode Änderungen gemacht wurden Herr Brill als Aut hor hinterlegt

--- src/numbers/Zahl.java
+++ src/numbers/Zahl.java
@@ -1,5 +1,16 @@
 package numbers;
 
+/**
+ * Eine Abstrakte Klasse.
+ * 
+ * <p> Durch die Klasse wird vorgegeben, dass die 4 Grundrechenarten und die Ausgabe der Zahl
+ * implementiert werden müssen. </p>
+ * 
+ * <P>Letzte Änderung: $Date: 2018/09/10 $</p> 
+ * @author $Author: brill $
+ * @version $Revision: #1 $
+ */
+
 public abstract class Zahl {
 	
 	 //Jede Zahl sollte mit den 4 Grundrechenarten bearbeitet werden können.	
